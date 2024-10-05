909d9dde4385ffcf9fd59b3fc348cd8ed851ec73
j0voda <vodajoaquin@gmail.com>
2024-10-05 15:08:14 -0300

    UserFormData con validaciones

---
+++ TPWebForms_Grupo21B/UserFormData.aspx
@@ -0,0 +1,79 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" MasterPageFile="~/Site.Master" CodeBehind="UserFormData.aspx.cs" Inherits="TPWebForms_Grupo21B.UserFormData" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <main>
+        <section class="row" aria-labelledby="aspnetTitle">
+            <h1 id="aspnetTitle">Completá el formulario con tus datos.</h1>
+        </section>
+
+        <asp:Label runat="server" CssClass="control-label mb-1" AssociatedControlID="dni">DNI: </asp:Label>
+        <asp:TextBox ID="dni" runat="server" MaxLength="8" CssClass="form-control" ClientIDMode="Static" placeholder="99999999"></asp:TextBox>
+        <asp:Label runat="server" ID="dniError" CssClass="invalid-feedback"></asp:Label>
+
+        <br/>
+
+        <div class="row">
+            <div class="col-3">
+                <asp:Label runat="server" CssClass="control-label mb-1" AssociatedControlID="name">Nombre: </asp:Label>
+                <asp:TextBox ID="name" runat="server" CssClass="form-control" ClientIDMode="Static" MaxLength="30" OnTextChanged="name_TextChanged"></asp:TextBox>
+                <asp:Label runat="server" ID="nameError" CssClass="invalid-feedback"></asp:Label>
+            </div>
+
+            <div class="col-3">
+                <asp:Label runat="server" CssClass="control-label mb-1" AssociatedControlID="surname">Apellido: </asp:Label>
+                <asp:TextBox ID="surname" runat="server" CssClass="form-control" ClientIDMode="Static" MaxLength="30"></asp:TextBox>
+                <asp:Label runat="server" ID="surnameError" CssClass="invalid-feedback"></asp:Label>
+            </div>
+
+            <div class="col-3">
+                <asp:Label runat="server" CssClass="control-label mb-1" AssociatedControlID="email">Email: </asp:Label>
+                <asp:TextBox ID="email" runat="server" CssClass="form-control" ClientIDMode="Static" AutoCompleteType="Email" MaxLength="30"></asp:TextBox><!--placeholder="email@email.com"-->
+                <asp:Label runat="server" ID="emailError" CssClass="invalid-feedback"></asp:Label>
+            </div>
+        </div>
+
+        <br/>
+
+        <div class="row">
+            <div class="col-3">
+                <asp:Label runat="server" CssClass="control-label mb-1" AssociatedControlID="address">Dirección: </asp:Label>
+                <asp:TextBox runat="server" ID="address" Width="100%" CssClass="form-control" ClientIDMode="Static" placeholder="Calle 123" MaxLength="30"></asp:TextBox>
+                <asp:Label runat="server" ID="addressError" CssClass="invalid-feedback"></asp:Label>
+            </div>
+
+            <div class="col-3">
+                <asp:Label runat="server" CssClass="control-label mb-1" AssociatedControlID="city">Ciudad: </asp:Label>
+                <asp:TextBox ID="city" runat="server" CssClass="form-control" ClientIDMode="Static" MaxLength="30"></asp:TextBox>
+                <asp:Label runat="server" ID="cityError" CssClass="invalid-feedback"></asp:Label>
+            </div>
+
+            <div class="col-2">
+                <asp:Label runat="server" CssClass="control-label mb-1" AssociatedControlID="cp">Código Postal: </asp:Label>
+                <asp:TextBox ID="cp" runat="server" CssClass="form-control" MaxLength="4" ClientIDMode="Static" placeholder="9999"></asp:TextBox>
+                <asp:Label runat="server" ID="cpError" CssClass="invalid-feedback"></asp:Label>
+            </div>
+        </div>
+
+        <br />
+
+        <div class="row">
+            <div class="col-4">
+                <asp:CheckBox runat="server" ID="accept" Text="&nbsp; He leído y acepto los términos y condiciones." TextAlign="Right" OnCheckedChanged="accept_CheckedChanged"/>
+                <asp:Label runat="server" ID="acceptError" CssClass="invalid-feedback"></asp:Label>
+            </div>
+        </div>
+
+        <br />
+
+        <div class="row">
+            <div class="col-2">
+                <asp:Button ID="btnSubmit" runat="server" Text="Participar" CssClass="btn btn-primary mt-2" OnClick="btnSubmit_Click"/>
+            </div>
+        </div>
+
+        <div class="row">
+            <h1>Results</h1>
+            <p><asp:Label runat="server" ID="labelResults"></asp:Label></p>
+        </div>
+    </main>
+</asp:Content>
---
+++ TPWebForms_Grupo21B/UserFormData.aspx.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+using System.Configuration;
+using System.Net.Mail;
+using System.Net;
+
+namespace TPWebForms_Grupo21B
+{
+    public partial class UserFormData : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack)
+            {
+                validateDni();
+                validateName();
+                validateSurname();
+                validateEmail();
+                validateAddress();
+                validateCity();
+                validateCP();
+                validateAccept();
+            }
+        }
+
+        protected void accept_CheckedChanged(object sender, EventArgs e)
+        {
+            //Hola
+            bool accepted = accept.Checked;
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            validateDni();
+            validateName();
+            validateSurname();
+            validateEmail();
+            validateAddress();
+            validateCity();
+            validateCP();
+            validateAccept();
+            labelResults.Text = dni.Text + " " + name.Text + " " + surname.Text;
+        }
+
+        protected void name_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private string addIsInvalidClass(string classes)
+        {
+            if (!classes.Contains("is-valid")) return classes + " is-invalid";
+
+            return classes;
+        }
+
+        private string removeIsInvalidClass(string classes)
+        {
+            return classes.Replace("is-invalid", "");
+        }
+
+        private void validateDni()
+        {
+            string dnitxt = this.dni.Text.Trim();
+            if (string.IsNullOrEmpty(dnitxt))
+            {
+                this.dni.CssClass = addIsInvalidClass(this.name.CssClass);
+                this.dniError.Text = "Debe completar el campo.";
+            }
+            else if (!dnitxt.All(char.IsDigit) || dnitxt.Length < 8)
+            {
+                this.dni.CssClass = addIsInvalidClass(this.dni.CssClass);
+                this.dniError.Text = "Ingrese 8 caracteres (solo números).";
+            }else
+            {
+                this.dni.CssClass = removeIsInvalidClass(this.dni.CssClass);
+            }
+        }
+
+        private void validateName()
+        {
+            string nametxt = this.name.Text.Trim();
+            if (string.IsNullOrEmpty(nametxt))
+            {
+                this.name.CssClass = addIsInvalidClass(this.name.CssClass);
+                this.nameError.Text = "Debe completar el campo.";
+            }
+            else if (nametxt.Any(char.IsDigit) || !nametxt.All(char.IsLetter))
+            {
+                this.name.CssClass = addIsInvalidClass(this.name.CssClass);
+                this.nameError.Text = "Ingrese solo letras.";
+            }else
+            {
+                this.name.CssClass = removeIsInvalidClass(this.name.CssClass);
+            }
+        }
+
+        private void validateSurname()
+        {
+            string surnametxt = this.surname.Text.Trim();
+            if (string.IsNullOrEmpty(surnametxt))
+            {
+                this.surname.CssClass = addIsInvalidClass(this.surname.CssClass);
+                this.surnameError.Text = "Debe completar el campo.";
+            }
+            else if (surnametxt.Any(char.IsDigit) || !surnametxt.All(char.IsLetter))
+            {
+                this.surname.CssClass = addIsInvalidClass(this.surname.CssClass);
+                this.surnameError.Text = "Ingrese solo letras.";
+            }
+            else
+            {
+                this.surname.CssClass = removeIsInvalidClass(this.surname.CssClass);
+            }
+        }
+
+        private void validateEmail()
+        {
+            var trimmedEmail = this.email.Text.Trim();
+            var email = new EmailAddressAttribute();
+
+            if (string.IsNullOrEmpty(trimmedEmail))
+            {
+                this.email.CssClass = addIsInvalidClass(this.email.CssClass);
+                this.emailError.Text = "Debe completar el campo.";
+            }else if (!email.IsValid(trimmedEmail))
+            {
+                this.email.CssClass = addIsInvalidClass(this.email.CssClass);
+                this.emailError.Text = "Email no válido.";
+            }
+            else
+            {
+                this.email.CssClass = removeIsInvalidClass(this.email.CssClass);
+            }
+        }
+
+        private void validateAddress()
+        {
+            // Validate street address
+            Regex validateStreetAddressRegex = new Regex("^(\\d{1,}) [a-zA-Z0-9\\s]+(\\,)? [a-zA-Z]+(\\,)? [A-Z]{2} [0-9]{5,6}$");
+             if (string.IsNullOrEmpty(this.address.Text.Trim()))
+            {
+                this.address.CssClass = addIsInvalidClass(this.address.CssClass);
+                this.addressError.Text = "Debe completar el campo.";
+            }else if (!validateStreetAddressRegex.IsMatch(this.address.Text.Trim()))
+            {
+                this.address.CssClass = addIsInvalidClass(this.address.CssClass);
+                this.addressError.Text = "Dirección no válida.";
+            }
+            else
+            {
+                this.address.CssClass = removeIsInvalidClass(this.address.CssClass);
+            }
+        }
+
+        private void validateCity()
+        {
+            string cityTxt = this.city.Text.Trim();
+
+            if (string.IsNullOrEmpty(cityTxt))
+            {
+                this.city.CssClass = addIsInvalidClass(this.city.CssClass);
+                this.cityError.Text = "Debe completar el campo.";
+            }
+            else if (cityTxt.Any(char.IsDigit) || !cityTxt.All(char.IsLetter))
+            {
+                this.city.CssClass = addIsInvalidClass(this.city.CssClass);
+                this.cityError.Text = "Ciudad no válida.";
+            }
+            else
+            {
+                this.city.CssClass = removeIsInvalidClass(this.city.CssClass);
+            }
+        }
+
+        private void validateCP()
+        {
+            string cpTxt = this.cp.Text.Trim();
+
+            if (string.IsNullOrEmpty(cpTxt))
+            {
+                this.cp.CssClass = addIsInvalidClass(this.cp.CssClass);
+                this.cpError.Text = "Debe completar el campo.";
+            }
+            else if (!cpTxt.All(char.IsDigit) || cpTxt.Length < 4)
+            {
+                this.cp.CssClass = addIsInvalidClass(this.cp.CssClass);
+                this.cpError.Text = "Código postal no válido.";
+            }
+            else
+            {
+                this.cp.CssClass = removeIsInvalidClass(this.cp.CssClass);
+            }
+        }
+
+        private void validateAccept()
+        {
+            if (!this.accept.Checked)
+            {
+                this.accept.CssClass = addIsInvalidClass(this.accept.CssClass);
+                this.acceptError.Text = "Debe aceptar los términos y condiciones.";
+            }
+            else
+            {
+                this.accept.CssClass = removeIsInvalidClass(this.accept.CssClass);
+            }
+        }
+
+        private void sendMail(string email, string nombreCompleto)
+        {
+            try
+            {
+                var fromEmailAddress = ConfigurationManager.AppSettings["FromEmailAddress"].ToString();
+                var fromEmailDisplayName = ConfigurationManager.AppSettings["FromEmailDisplayName"].ToString();
+                var fromEmailPassword = ConfigurationManager.AppSettings["FromEmailPassword"].ToString();
+                var smtpHost = ConfigurationManager.AppSettings["SMTPHost"].ToString();
+                var smtpPort = ConfigurationManager.AppSettings["SMTPPort"].ToString();
+
+                string body = "Su registro para la promoción del grupo 21 B ha sido realizada con éxito. ¡Gracias!.";
+                MailMessage message = new MailMessage(new MailAddress(fromEmailAddress, fromEmailDisplayName), new MailAddress(email, nombreCompleto));
+                message.Subject = "Registro exitoso.";
+                message.IsBodyHtml = true;
+                message.Body = body;
+
+                var client = new SmtpClient();
+                client.Credentials = new NetworkCredential(fromEmailAddress, fromEmailPassword);
+                client.Host = smtpHost;
+                client.EnableSsl = true;
+                client.Port = !string.IsNullOrEmpty(smtpPort) ? Convert.ToInt32(smtpPort) : 0;
+                client.Send(message);
+            }
+            catch (Exception ex)
+            {
+                throw (new Exception("Error al enviar email."));
+            }
+        }
+    }
+}
